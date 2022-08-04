2370dd3b0cd810911b1ddce3e4444877ffef7a29
Byeon Jinsang <quswlstkd88@naver.com>
2022-08-04 13:13:06 +0900

    8.4

--- implementation/2_2_implementation_timeCountThree.py
+++ implementation/2_2_implementation_timeCountThree.py
@@ -13,8 +13,14 @@ for i in range(0, n + 1):
         m = str(j)
         for k in range(0, 60):
             s = str(k)
+            '''
             time = h + m + s
             if list(time).count("3") > 0:
                 answer += 1
+            '''
+            # 위의 코드를 아래와 같이 줄일 수 있다. 그런데 내 코드도 직관성에서는 나쁘지 않은 것 같다.
+            if '3' in str(i) + str(j) + str(k):
+                answer += 1
+
 
 print(answer)
